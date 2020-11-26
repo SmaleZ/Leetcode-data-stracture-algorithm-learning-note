d003763e65b7a6d9a6a31b21909d2b11e6271e2b
Ziligit <34412996+SmaleZ@users.noreply.github.com>
2020-11-26 21:57:15 +0100

    Update k个一组翻转链表.py

--- 数据结构：数组与链表/k个一组翻转链表.py
+++ 数据结构：数组与链表/k个一组翻转链表.py
@@ -6,6 +6,7 @@ k 是一个正整数，它的值小于或等于链表的长度。
 
 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
 '''
+//解题重点： 链表翻转，prehead node
 class Solution:
     # 翻转一个子链表，并且返回新的头与尾
     def reverse(self, head: ListNode, tail: ListNode):
