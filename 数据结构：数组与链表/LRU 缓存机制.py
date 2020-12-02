30f5fe4b2831f550f343d1bb87d31afc8f4559bf
Ziligit <34412996+SmaleZ@users.noreply.github.com>
2020-12-02 16:56:06 +0100

    Create LRU 缓存机制.py

---
+++ 数据结构：数组与链表/LRU 缓存机制.py
@@ -0,0 +1,78 @@
+'''
+运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制 。
+实现 LRUCache 类：
+
+LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存
+int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
+'''
+
+\\ 知识点： 哈希表， 双链表。利用哈希表O（1）来查询获取缓存里的值， 利用双链表更新数据使用的情况，将最新访问的数据放在表头
+class DlinedNode:
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+				//The Python dict() is a built-in function that returns a dictionary object or simply creates a dictionary in Python.
+        self.cache = dict()
+        self.head = DlinedNode()
+        self.tail = DlinedNode()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.capacity = capacity
+        self.size = 0
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self.moveToHead(node)
+        return node.value
+
+    def put(self, key: int, value: int) -> None:
+        if key not in self.cache:
+            node = DlinedNode(key, value)
+            self.cache[key] = node
+						//将新加入的元素放在表头
+            self.addToHead(node)
+            self.size +=1
+            if self.size > self.capacity:
+								//如果超过缓存容量，则将表尾数据剔除
+                removed = self.removeTail()
+                self.cache.pop(removed.key)
+                self.size -= 1
+        else:
+            node = self.cache[key]
+            node.value = value
+            self.moveToHead(node)
+    
+    def addToHead(self, node):
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+    
+    def removeNode(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def moveToHead(self, node):
+        self.removeNode(node)
+        self.addToHead(node)
+    
+    def removeTail(self):
+        node = self.tail.prev
+        self.removeNode(node)
+        return node
+
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
